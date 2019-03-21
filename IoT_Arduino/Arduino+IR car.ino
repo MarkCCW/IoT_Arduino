80ac73ec9ecec99174761e6fbf22b523a3c80d88
MarkCCW <48026214+MarkCCW@users.noreply.github.com>
2019-03-21 13:56:10 +0800

    Add files via upload

---
+++ IoT_Arduino/Arduino+IR car.ino
@@ -0,0 +1,115 @@
+#include <IRremote.h>
+
+const int ENA = 5;
+const int ENB = 10;
+const int In1 = 6;              
+const int In2 = 7;
+const int In3 = 8;
+const int In4 = 9;
+
+int irReceiver = 2;
+IRrecv irrecv(irReceiver);
+decode_results results;
+
+void setup() {
+  Serial.begin(9600);
+  irrecv.enableIRIn();
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  pinMode(In1, OUTPUT);
+  pinMode(In2, OUTPUT);
+  pinMode(In3, OUTPUT);
+  pinMode(In4, OUTPUT);
+}
+
+void IRaction(int IRcode)
+{
+     switch(IRcode)
+     {
+       case 0xFF629D: 
+         Goahead(); 
+         break;
+       case 0xFFA857: 
+         Goback(); 
+         break;
+       case 0xFF22DD: 
+         Goleft();  
+         break;
+       case 0xFFC23D: 
+         Goright(); 
+         break;
+       case 0xFF02FD: 
+         Carstop(); 
+         break;
+     }
+}
+
+void Goahead(){
+  digitalWrite(In1,HIGH);
+  digitalWrite(In2,LOW);
+  digitalWrite(In3,HIGH);
+  digitalWrite(In4,LOW);
+  
+  analogWrite(ENA,200);
+  analogWrite(ENB,200);
+}
+
+void Goback(){
+  digitalWrite(In1,LOW);
+  digitalWrite(In2,HIGH);
+  digitalWrite(In3,LOW);
+  digitalWrite(In4,HIGH);
+  
+  analogWrite(ENA,200);
+  analogWrite(ENB,200);
+}
+
+void Goleft(){
+  digitalWrite(In1,LOW);
+  digitalWrite(In2,LOW);
+  digitalWrite(In3,HIGH);
+  digitalWrite(In4,LOW);
+  
+  analogWrite(ENA,200);
+  analogWrite(ENB,200);
+}
+
+void Goright(){
+  digitalWrite(In1,HIGH);
+  digitalWrite(In2,LOW);
+  digitalWrite(In3,LOW);
+  digitalWrite(In4,LOW);
+  
+  analogWrite(ENA,200);
+  analogWrite(ENB,200);
+}
+
+void Carstop(){
+  digitalWrite(In1,LOW);
+  digitalWrite(In2,LOW);
+  digitalWrite(In3,LOW);
+  digitalWrite(In4,LOW);
+  
+  analogWrite(ENA,0);
+  analogWrite(ENB,0);
+}
+
+void loop()
+{
+  Goahead();
+  delay(1000);
+  Goback();
+  delay(1000);
+  Goleft();
+  delay(1000);
+  Goright();
+  delay(1000);
+  Carstop();
+  delay(5000);
+}
+
+
+
+
+
+
---
+++ IoT_Arduino/wifi_connection.ino
@@ -0,0 +1,105 @@
+﻿#include "DHT.h"    // 使用 ESP8266 自帶的 DHT11 函式庫
+                    // 如果 libraries 已有其他的 DHT 函式庫，請先移除
+#include <ESP8266WiFi.h>
+
+//*-- Hardware Serial
+#define _baudrate   9600
+
+//*-- DHT11
+#define _dhtpin     D8
+#define _dhttype    DHT11
+
+// Initialize DHT sensor 
+DHT dht11( _dhtpin, _dhttype );
+float temperature, humidity;
+
+//*-- IoT Information
+#define SSID    "分享器ID名稱"
+#define PASS    "密碼"
+#define HOST    "184.106.153.149" // ThingSpeak IP Address: 184.106.153.149//api.thingspeak.com
+#define PORT    80
+// 使用 GET 傳送資料的格式
+// GET /update?key=[THINGSPEAK_KEY]&field1=[data 1]&filed2=[data 2]...;
+String GET = "GET /update?key=0VD8RROGTK2MTIFT";
+
+void setup() {
+    Serial.begin( _baudrate );
+    Serial.println( "ESP8266 Ready!" );
+    // Connecting to a WiFi network
+    Serial.print("Connect to ");
+    Serial.println( SSID );
+    WiFi.begin( SSID, PASS );
+
+    // 持續等待並連接到指定的 WiFi SSID
+    while( WiFi.status() != WL_CONNECTED )
+    {
+        delay(500);
+        Serial.print( "." );
+    }
+    Serial.println( "" );
+
+    Serial.println( "WiFi connected" );
+    Serial.println( "IP address: " );
+    Serial.println( WiFi.localIP() );
+    Serial.println( "" );
+    
+    // DHT11
+    dht11.begin();
+    Serial.println( "DHT11 Ready!" );
+    delay(2000);
+}
+
+void loop() {
+    humidity    = dht11.readHumidity();
+    temperature = dht11.readTemperature();
+
+    // 確認取回的溫溼度數據可用
+    if( isnan( humidity ) || isnan( temperature ) )
+    {
+        Serial.println( "Failed to read form DHT11" );
+    return;
+    }
+    else
+    {
+        // DHT11 溫度與濕度傳送      
+        Serial.print( "Humidity: " );
+        Serial.print( humidity );
+        Serial.print( ", Temperature: " );
+        Serial.println( temperature );
+        updateDHT11();
+    }
+
+    // 每隔多久傳送一次資料
+    delay( 20000 ); // 20 second
+}
+
+void updateDHT11()
+{
+    // 設定 ESP8266 作為 Client 端
+    WiFiClient client;
+    if( !client.connect( HOST, PORT ) )
+    {
+        Serial.println( "connection failed" );
+        return;
+    }
+    else
+    {
+        // 準備上傳到 ThingSpeak IoT Server 的資料
+        // 已經預先設定好 ThingSpeak IoT Channel 的欄位
+        // field1：溫度；field2：濕度
+        String getStr = GET + "&field1=" + String((int)temperature) + 
+                              "&field2=" + String((int)humidity) +
+                              " HTTP/1.1\r\n";;
+        client.print( getStr );
+        client.print( "Host: api.thingspeak.com\n" );
+        client.print( "Connection: close\r\n\r\n" );
+        
+        delay(10);
+        //
+        // 處理遠端伺服器回傳的訊息，程式碼可以寫在這裡！
+        //
+
+
+        client.stop();
+    }
+}
